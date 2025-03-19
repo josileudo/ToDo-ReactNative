533cf311a9729f6b6d45f90fc598fb7cc96c78b2
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-03-19 18:19:30 -0300

    :memo: feat: Implement README to application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
+import {Text} from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Text>Hello guys</Text>
+  )
 }
